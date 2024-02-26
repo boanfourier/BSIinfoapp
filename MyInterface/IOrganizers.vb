1939d1ed248d929bcc837f8ca8022444d23814f6
boanfourier <89958959+boanfourier@users.noreply.github.com>
2024-02-26 09:32:27 +0700

    Delete MyInterface directory

--- MyInterface/IOrganizers.vb
+++
@@ -1,7 +0,0 @@
-﻿
-Imports BSI_info.BO.ConsoleApp1
-Public Interface IOrganizers
-    Sub AddOrganizer(ByVal organizer As Organizer)
-    Function GetAllOrganizer() As List(Of Organizer)
-
-End Interface
